0d66d49403bff26a6ea3e352901f52d18943d9d9
Allen OYANG <allenwill211@gmail.com>
2022-10-24 09:28:19 +0800

    change: change ui

--- libs/components/markdown/src/md/MarkdownRender.tsx
+++ libs/components/markdown/src/md/MarkdownRender.tsx
@@ -1,6 +1,6 @@
 /*
  * @Date: 2022-10-22 17:20:08
- * @LastEditTime: 2022-10-22 21:54:00
+ * @LastEditTime: 2022-10-23 23:45:22
  */
 import { FC } from 'react';
 import Markdown from 'react-markdown'
@@ -11,6 +11,7 @@ import styled from '@emotion/styled';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
 import { atomDark } from 'react-syntax-highlighter/dist/esm/styles/prism'
 import { css } from '@emotion/react'
+import { COLOR } from '@beginalive/components/ui';
 
 interface MarkdownRenderCompProps {
 }
@@ -20,6 +21,8 @@ export const MarkdownRenderComp: FC<MarkdownRenderCompProps> = (props) => {
 
   const renderers = {
     code: CodeComp,
+    em: EmComp,
+    // strong: strongComp
   };
   return (
     <Container>
@@ -27,6 +30,31 @@ export const MarkdownRenderComp: FC<MarkdownRenderCompProps> = (props) => {
     </Container>
   )
 }
+
+
+const strongComp = ({ node, inline, className, children, ...props }: any) => {
+  return (
+    <StrongCompUI className={className} {...props}>
+      {children}
+    </StrongCompUI>
+  )
+}
+const StrongCompUI = styled('strong')({
+  fontSize: 12
+})
+
+
+const EmComp = ({ node, inline, className, children, ...props }: any) => {
+  return (
+    <EmCompUI className={className} {...props}>
+      {children}
+    </EmCompUI>
+  )
+}
+
+const EmCompUI = styled('em')({
+  color: '#ec8484'
+})
 
 const CodeComp = ({ node, inline, className, children, ...props }: any) => {
   const match = /language-(\w+)/.exec(className || '')
@@ -39,7 +67,12 @@ const CodeComp = ({ node, inline, className, children, ...props }: any) => {
       {...props}
     />
   ) : (
-    <code className={className} {...props}>
+    <code className={className} {...props} style={{
+        borderRadius: '5px',
+        padding: '0 5px',
+        background: `rgb(105 105 105)`
+      }}
+    >
       {children}
     </code>
   )
